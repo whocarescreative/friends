eec3b613484401d9a8afb956413a0ea842cee168
Max Kurapov <maxkurapov@gmail.com>
2019-06-10 21:29:48 +0200

    text file method

--- run.py
+++ run.py
@@ -14,61 +14,147 @@ from bs4 import BeautifulSoup
 baseUrl = 'https://fangj.github.io/friends/season/'
 
 soup = None
-
-
-
-def extractLines():
-    for p in soup.find_all('p'):
-        actionPattern = '\([^)]*\)'
-        cleanLine = re.sub(actionPattern, "", p.getText())
-        b = p.find('b')
-        if b is not None:
-            b_text = b.get_text()
-
-            if ':' in b_text:
-
-                utterance = cleanLine.split(':',1)[1].strip()
-                actor = b_text.replace(":"," ").strip()
-
-                line = {
-                    'actor': actor,
-                    'text': utterance
-                }
-                lines.append(line)
-
-
-lines = []
+episode_count = 0
 
 episodes = []
+seasons = []
 
-season = '02'
-for i in range(1, 25):
-    url = baseUrl+ f'{season}{str(i).zfill(2)}.html'
+season_num = 0
+episode_num = 0
 
-    print(url)
+
+def season2Check(speaker):
+    if speaker == 'RUSS':
+        speaker = 'Ross'
+
+    if speaker == 'MNCA':
+        speaker = 'Monica'
+        
+    if speaker == 'CHAN':
+        speaker = 'Chandler'
+
+    if speaker == 'RACH':
+        speaker = 'Rachel'
+
+    if speaker == 'JOEY':
+        speaker.capitalize()
     
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    lines = []
-    extractLines()
+    if speaker == 'PHOE':
+        speaker = 'Phoebe'
+    return speaker
 
-    episode = {
-        'num':i,
-        'lines':lines
-    }
 
-    episodes.append(episode)
 
-with open(f'data{season}.json', 'w') as out:
-    datadump = {
-        "data": {
-            'seasons': {
-                'num':1,
-                'episdodes': episodes
+def massageLine(line):
+
+    speaker = None
+    utterance = None
+
+    actionPattern = "[\(\[].*?[\)\]]"
+    cleanLine = re.sub(actionPattern, "", line)
+
+    # print(cleanLine)
+
+    if ':' in cleanLine:
+        speaker = cleanLine.split(':',1)[0].strip()
+        speaker = speaker.lower()
+
+        if not speaker.endswith(" by") and not "commercial voiceover" in speaker:
+            utterance = cleanLine.split(':',1)[1].strip()
+            speaker = speaker.replace(":"," ").strip()
+
+            if int(season_num) is 2:
+                speaker = season2Check(speaker)
+
+            return {
+                'speaker': speaker,
+                'text': utterance
             }
-        }
-    }
-    json.dump(datadump, out)
+
+    return None
+
+
+def run():
+
+    global episode_count
+    global season_num
+    global episode_num
+
+    url = None
+    if (season_num == 9 and episode_num == 23):
+        url = 'https://fangj.github.io/friends/season/0923-0924.html'
+    elif (season_num == 10 and episode_num == 17):
+        url = 'https://fangj.github.io/friends/season/1017-1018.html'
+    elif (season_num == 6 and episode_num == 15):
+        url = 'https://fangj.github.io/friends/season/0615-0616.html'
+    elif (season_num == 2 and episode_num == 12):
+        url = 'https://fangj.github.io/friends/season/0212-0213.html'
+    else:
+        url = baseUrl+ f'{str(season_num).zfill(2)}{str(episode_num).zfill(2)}.html'
    
+    response = requests.get(url)
+
+    
+    if response.status_code == 200:
+        print(url)
+        html = response.text
+
+        text_maker = html2text.HTML2Text()
+        text_maker.ignore_emphasis = True
+
+        text = text_maker.handle(html)
+
+        if season_num == 2 and episode_num >= 12:
+            formatted_text = text.replace('  ','').replace('\n\n','**').replace('\n',' ').replace('**','\n')
+        else:
+            formatted_text = text.replace('\n\n','**').replace('\n',' ').replace('**','\n')
 
 
+        episode_count = episode_count + 1
+        episode_lines = []
+
+        file1 = open("myfile.txt","w")#write mode 
+        file1.write(formatted_text) 
+        file1.close() 
+
+        with open("myfile.txt") as fp:  
+            line = fp.readline()
+
+            while line:
+                if (line == '\n'):
+                    print('********')
+                else:
+                    line_obj = massageLine(line)
+                    if (line_obj is not None):
+                        episode_lines.append(line_obj)
+
+                line = fp.readline()
+
+
+        episode = {
+            'num':episode_count,
+            'lines':episode_lines
+        }
+
+        episodes.append(episode)
+
+
+for i in range (2,3):
+    season_num = i
+    episodes = []
+
+    for j in range(10, 27):
+        episode_num = j 
+        run()
+
+    season = {
+        'num': season_num,
+        'episodes':episodes
+    }
+    seasons.append(season)
+    with open(f'data{season_num}.json', 'w') as out:
+        datadump = {
+            "data": season
+        }
+        json.dump(datadump, out)
+    
