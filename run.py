b3397edfead27e9d451c5bcf2109dc73faa890e9
Max Kurapov <maxkurapov@gmail.com>
2019-06-10 17:59:33 +0200

    season 1 scraped

--- run.py
+++ run.py
@@ -11,67 +11,60 @@ import html2text
 from bs4 import BeautifulSoup
 
 # Set the URL you want to webscrape from
-url = 'https://fangj.github.io/friends/season/0110.html'
+baseUrl = 'https://fangj.github.io/friends/season/'
 
-# Connect to the URL
-response = requests.get(url)
-
-# Parse HTML and save to BeautifulSoup object¶
-soup = BeautifulSoup(response.text, "html.parser")
-
-# print(soup.prettify())
+soup = None
 
 
-# file2 = open("dump.txt","w+") 
-# text = html2text.html2text(response.text)
-# file2.write(text)
 
-data = {
-    'seasons':[]
-}
 
-lines = []
+# def extractLines():
+#     for p in soup.find_all('p'):
+#         actionPattern = '\([^)]*\)'
+#         cleanLine = re.sub(actionPattern, "", p.getText())
+#         b = p.find('b')
+#         if b is not None:
+#             b_text = b.get_text()
 
-for p in soup.find_all('p'):
-    actionPattern = '\([^)]*\)'
-    cleanLine = re.sub(actionPattern, "", p.getText())
-    b = p.find('b')
-    if b is not None:
-        b_text = b.get_text()
-        # print(b_text)
+#             if ':' in b_text:
+#                 print(b_text)
 
-        utterance = cleanLine.split(b_text,1)[1].strip()
-        actor = b_text.replace(":"," ").strip()
+#                 utterance = cleanLine.split(':',1)[1].strip()
+#                 actor = b_text.replace(":"," ").strip()
 
-        line = {
-            'actor': actor,
-            'text': utterance
-        }
-        lines.append(line)
+#                 line = {
+#                     'actor': actor,
+#                     'text': utterance
+#                 }
+#                 lines.append(line)
 
-with open('season1.json', 'w') as out:
-    datadump = {
-        "data": lines
-    }
-    json.dump(datadump, out)
+
+# lines = []
+# episodes = []
+# for i in range(1, 25):
+#     url = baseUrl+ f'01{str(i).zfill(2)}.html'
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, "html.parser")
+#     print(i)
+#     extractLines()
+
+#     episode = {
+#         'num':i,
+#         'lines':lines
+#     }
+
+#     episodes.append(episode)
+
+# with open('data1.json', 'w') as out:
+#     datadump = {
+#         "data": {
+#             'seasons': {
+#                 'num':1,
+#                 'episdodes': episodes
+#             }
+#         }
+#     }
+#     json.dump(datadump, out)
    
 
 
-
-# with open("dump.txt")  as f:
-#     for line in f:
-#         if line.strip():
-#             actionPattern = '.*?\((.*?)\)'
-#             action = re.findall(actionPattern, line)
-
-#             test = re.sub(actionPattern, "", line)
-#             print(test)
-
-#             # if (len(action) > 0):
-#             #     print(action)
-
-#             namePattern = '\*{2}(.*):\*{2}'
-#             name = re.findall(namePattern, line)
-
-#             # namePattern = line.split("**",1)[1] 
-            # text = 
