4244c31c2ddeada98129a8dc4b0fb0c29f3ce0b6
Max Kurapov <maxkurapov@gmail.com>
2019-06-10 18:21:51 +0200

    almost there

--- run.py
+++ run.py
@@ -17,54 +17,58 @@ soup = None
 
 
 
+def extractLines():
+    for p in soup.find_all('p'):
+        actionPattern = '\([^)]*\)'
+        cleanLine = re.sub(actionPattern, "", p.getText())
+        b = p.find('b')
+        if b is not None:
+            b_text = b.get_text()
 
-# def extractLines():
-#     for p in soup.find_all('p'):
-#         actionPattern = '\([^)]*\)'
-#         cleanLine = re.sub(actionPattern, "", p.getText())
-#         b = p.find('b')
-#         if b is not None:
-#             b_text = b.get_text()
+            if ':' in b_text:
 
-#             if ':' in b_text:
-#                 print(b_text)
+                utterance = cleanLine.split(':',1)[1].strip()
+                actor = b_text.replace(":"," ").strip()
 
-#                 utterance = cleanLine.split(':',1)[1].strip()
-#                 actor = b_text.replace(":"," ").strip()
-
-#                 line = {
-#                     'actor': actor,
-#                     'text': utterance
-#                 }
-#                 lines.append(line)
+                line = {
+                    'actor': actor,
+                    'text': utterance
+                }
+                lines.append(line)
 
 
-# lines = []
-# episodes = []
-# for i in range(1, 25):
-#     url = baseUrl+ f'01{str(i).zfill(2)}.html'
-#     response = requests.get(url)
-#     soup = BeautifulSoup(response.text, "html.parser")
-#     print(i)
-#     extractLines()
+lines = []
 
-#     episode = {
-#         'num':i,
-#         'lines':lines
-#     }
+episodes = []
 
-#     episodes.append(episode)
+season = '02'
+for i in range(1, 25):
+    url = baseUrl+ f'{season}{str(i).zfill(2)}.html'
 
-# with open('data1.json', 'w') as out:
-#     datadump = {
-#         "data": {
-#             'seasons': {
-#                 'num':1,
-#                 'episdodes': episodes
-#             }
-#         }
-#     }
-#     json.dump(datadump, out)
+    print(url)
+    
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    lines = []
+    extractLines()
+
+    episode = {
+        'num':i,
+        'lines':lines
+    }
+
+    episodes.append(episode)
+
+with open(f'data{season}.json', 'w') as out:
+    datadump = {
+        "data": {
+            'seasons': {
+                'num':1,
+                'episdodes': episodes
+            }
+        }
+    }
+    json.dump(datadump, out)
    
 
 
